284990fc9ff8b299405f538a416009cbfd2ea5b9
P7ngu <60442992+P7ngu@users.noreply.github.com>
2025-07-16 11:16:04 +0200

    Added check for Mint

--- packages/foundry/contracts/TestContract.sol
+++ packages/foundry/contracts/TestContract.sol
@@ -6,10 +6,15 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract TestContract is ERC1155, Ownable {
+    uint8 public constant NUM_CARDS = 21; 
+    //21 cards in the collection, 8 bit - up to 255 cards, in case of future expansion, change it to uint16
+
+
     constructor(string memory uri_) ERC1155(uri_) Ownable(msg.sender) {}
 
     // Minting a single card, step by step reward
     function mint(address to, uint256 id, uint256 amount, bytes memory data) public onlyOwner {
+        require(id >= 1 && id <= NUM_CARDS, "Invalid card ID"); //Cards from 0 - the fool - to 21
         _mint(to, id, amount, data);
     }
 
