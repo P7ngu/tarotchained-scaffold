6537b95e673c4b85b55a075ba60328da222ffb03
P7ngu <60442992+P7ngu@users.noreply.github.com>
2025-07-16 23:51:15 +0200

    Added files

---
+++ packages/nextjs/components/scaffold-eth/Textbox.jsx
@@ -0,0 +1,67 @@
+import { isTextBoxVisibleAtom, textBoxContentAtom } from "~~/app/store/store.js";
+import { useAtomValue, useAtom } from "jotai";
+import "~~/styles/textbox.css"
+import { useEffect, useState } from "react";
+import {motion, scale} from "framer-motion";
+
+const variants = {
+    open : {opacity: 1, scale: 1},
+    closed: { opacity: 0, scale: 0.5},
+};
+
+export default function TextBox(){
+    const [isVisible, setIsVisible] = useAtom(isTextBoxVisibleAtom)
+    const content = useAtomValue(textBoxContentAtom);
+    const fixedHint = "cliccami";
+
+    const [isCloseRequest, setIsCloseRequest] = useState(false);
+
+    const handleAnimationComplete = () => {
+        if (isCloseRequest) {
+            setIsVisible(false);
+            setIsCloseRequest(false);
+        }
+    }
+
+    useEffect(() => {
+        const closeHandler = (e) => {
+            if (!isVisible) return;
+            if(e.code === "Space"){
+                setIsCloseRequest(true)
+            }
+        };
+
+        window.addEventListener("keydown", closeHandler);
+
+        return () => {
+            window.removeEventListener("keydown", closeHandler);
+        }
+    }, [isVisible]);
+
+    return (
+        isVisible && (
+        <>
+          <motion.img
+            className="speaker-img"
+            src="/omino.png"
+            alt="Speaker"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={isCloseRequest ? "closed" : "open"}
+            variants={variants}
+            transition={{ duration: 0.2 }}
+          />
+          <motion.div
+            className="text-box"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={isCloseRequest ? "closed" : "open"}
+            variants={variants}
+            transition={{ duration: 0.2 }}
+            onAnimationComplete={handleAnimationComplete}
+          >
+            <p>{content}</p>
+          </motion.div>
+          <p className="click-enter-hint">Click enter to continue...</p>
+        </>
+        )
+    );
+}
