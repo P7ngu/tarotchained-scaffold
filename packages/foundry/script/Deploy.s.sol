e33be0a165abb0f3968eb402e2709f1f99a81875
P7ngu <60442992+P7ngu@users.noreply.github.com>
2025-07-16 11:31:28 +0200

    Added init - to change - for uris test

--- packages/foundry/script/Deploy.s.sol
+++ packages/foundry/script/Deploy.s.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.19;
 
 import "./DeployHelpers.s.sol";
 import { DeployYourContract } from "./DeployYourContract.s.sol";
+import { uris } from "./cardUris.json";
 
 /**
  * @notice Main deployment script for all contracts
@@ -10,10 +11,16 @@ import { DeployYourContract } from "./DeployYourContract.s.sol";
  *
  * Example: yarn deploy # runs this script(without`--file` flag)
  */
+
+
 contract DeployScript is ScaffoldETHDeploy {
     function run() external {
         // Deploys all your contracts sequentially
         // Add new deployments here when needed
+        string memory initCode = "";
+        for (uint256 i = 0; i < uris.length; i++) {
+            initCode = string.concat(initCode, "cardUris[", Strings.toString(i), "] = ", uris[i], ";\n");
+        }
 
         DeployYourContract deployYourContract = new DeployYourContract();
         deployYourContract.run();
