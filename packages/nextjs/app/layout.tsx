1990a9da16cb9200a74f3e417f6bdf269756f930
Veronica Natale <116646462+ver38@users.noreply.github.com>
2025-07-16 18:31:58 +0200

    moved file in correct directory

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -3,6 +3,8 @@ import type { ReactNode } from "react";
 import AppWrapper from "~~/app/AppWrapper";
 import "~~/styles/index.css";
 
+//import { ScaffoldEthAppWithProviders } from "~~/components/scaffold-eth/ScaffoldEthAppWithProviders";
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
