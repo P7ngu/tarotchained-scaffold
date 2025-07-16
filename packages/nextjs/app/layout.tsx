bef8292e072f4abeeb018572405eba2cc45ba471
Veronica Natale <116646462+ver38@users.noreply.github.com>
2025-07-16 08:19:12 +0200

    cleaned project 1
    cleaned project by commenting default components to see if a anything breaks

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -1,21 +1,32 @@
 import "@rainbow-me/rainbowkit/styles.css";
-import { ScaffoldEthAppWithProviders } from "~~/components/ScaffoldEthAppWithProviders";
-import { ThemeProvider } from "~~/components/ThemeProvider";
+//import { ScaffoldEthAppWithProviders } from "~~/components/ScaffoldEthAppWithProviders";
+//import { ThemeProvider } from "~~/components/ThemeProvider";
 import "~~/styles/globals.css";
-import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
+//import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
+
+/*
 export const metadata = getMetadata({
   title: "Scaffold-ETH 2 App",
   description: "Built with 🏗 Scaffold-ETH 2",
-});
+});*/
 
-const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
+const ScaffoldEthApp = () => {
+  /*
   return (
     <html suppressHydrationWarning className={``}>
       <body>
         <ThemeProvider enableSystem>
           <ScaffoldEthAppWithProviders>{children}</ScaffoldEthAppWithProviders>
         </ThemeProvider>
+      </body>
+    </html>
+  );*/
+  //in questo return c'è la root del sito
+  return (
+    <html>
+      <body>
+        <div>blablabla</div>
       </body>
     </html>
   );
--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,14 +1,11 @@
 "use client";
 
-import Link from "next/link";
 import type { NextPage } from "next";
-import { useAccount } from "wagmi";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { Address } from "~~/components/scaffold-eth";
 
 const Home: NextPage = () => {
-  const { address: connectedAddress } = useAccount();
-
+  // const { address: connectedAddress } = useAccount();
+  return <div>ciao</div>;
+  /*
   return (
     <>
       <div className="flex items-center flex-col grow pt-10">
@@ -66,7 +63,7 @@ const Home: NextPage = () => {
         </div>
       </div>
     </>
-  );
+  ); */
 };
 
 export default Home;
