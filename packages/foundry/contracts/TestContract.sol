cbb28d3d75aff6d8415dbb4192f29412082bbab4
P7ngu <60442992+P7ngu@users.noreply.github.com>
2025-07-16 11:45:13 +0200

    Switched to hard coded Test uri + function override for original uri function of ERC1155

--- packages/foundry/contracts/TestContract.sol
+++ packages/foundry/contracts/TestContract.sol
@@ -9,8 +9,22 @@ contract TestContract is ERC1155, Ownable {
     uint8 public constant NUM_CARDS = 22; 
     //23 cards in the collection + the RGB one, 8 bit - up to 255 cards, in case of future expansion, change it to uint16
 
+    mapping(uint8 => string) public cardUri; //CardID to CardURI
 
-    constructor(string memory uri_) ERC1155(uri_) Ownable(msg.sender) {}
+    constructor(string memory uri_) ERC1155(uri_) Ownable(msg.sender) {
+        setupUrisForTesting();
+    }
+
+    function setupUrisForTesting () private {
+        //Must change this to be scalable and remove extra gas cost
+        cardUri[0]  = "ipfs://bafkreifu7wfdmwaiazl4xzpjdmvyt3cijzv3uzgyzq7wwanzgqfcruuqf4";
+    }
+     //Override ERC-1155’s URI lookup to use mapping, we must use uint256 here, but the func is view only
+    function uri(uint256 id) public view override returns (string memory) {
+        require(id <= NUM_CARDS, "Invalid card ID");
+        uint8 key = uint8(id); //Casting uint256 to uint8, for our mapping to require less space and gas cost
+        return cardUri[key];
+    }
 
     // Minting a single card, step by step reward
     function mint(address to, uint256 id, uint256 amount, bytes memory data) public onlyOwner {
--- packages/foundry/script/Deploy.s.sol
+++ packages/foundry/script/Deploy.s.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.19;
 
 import "./DeployHelpers.s.sol";
 import { DeployYourContract } from "./DeployYourContract.s.sol";
-import { uris } from "./cardUris.json";
 
 /**
  * @notice Main deployment script for all contracts
@@ -17,11 +16,7 @@ contract DeployScript is ScaffoldETHDeploy {
     function run() external {
         // Deploys all your contracts sequentially
         // Add new deployments here when needed
-        string memory initCode = "";
-        for (uint256 i = 0; i < uris.length; i++) {
-            initCode = string.concat(initCode, "cardUris[", Strings.toString(i), "] = ", uris[i], ";\n");
-        }
-
+      
         DeployYourContract deployYourContract = new DeployYourContract();
         deployYourContract.run();
 
